a4036d2d58ca9e81dbfcad48c3c4491c929c3a04
Samyeak <mail@samyeak.com.np>
2025-05-01 23:09:53 +0545

    created new project due to cf pages issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
