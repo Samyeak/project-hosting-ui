32a94a7b56e21c6f321a054d303743d4a5fe2dc7
Samyeak <mail@samyeak.com.np>
2025-05-01 22:55:35 +0545

    removed pacjage for cf eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import pluginNextOnPages from "eslint-plugin-next-on-pages";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,15 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  // Add next-on-pages plugin with recommended config
-  {
-    plugins: {
-      "next-on-pages": pluginNextOnPages,
-    },
-    rules: {
-      ...pluginNextOnPages.configs.recommended.rules,
-    },
-  },
 ];
 
 export default eslintConfig;
